87d9e58a88ab47a1e73fd43a8fb2c5dbc6bbbe2e
laurentiuasan <lau.lau95@yahoo.com>
2022-12-20 19:56:20 +0200

    Reset works, all movements added. #6

--- 2048/mainframe.py
+++ 2048/mainframe.py
@@ -5,12 +5,11 @@ import functions
 
 
 class Game(Frame):
-
+    # UI Part
     def __init__(self):
         Frame.__init__(self)
         self.master.title("2048 THE Game")
         self.master.geometry("{}x{}".format(e.WINDOW_SIZE, e.WINDOW_SIZE))
-        self.master.bind("<Key>", self.pressed_key)
         self.grid()
 
         self.board = []
@@ -19,13 +18,8 @@ class Game(Frame):
 
         self.init_grid()
 
-        self.commands = {
-            e.KEY_UP: functions.move(self.matrix, "up"),
-            e.KEY_DOWN: functions.move(self.matrix, "down"),
-            e.KEY_LEFT: functions.move(self.matrix, "left"),
-            e.KEY_RIGHT: functions.move(self.matrix, "right"),
-            e.KEY_RESET: functions.init_game()
-        }
+        self.master.bind("<Key>", self.move)
+        self.states.append(self.matrix)
 
         self.mainloop()
 
@@ -61,15 +55,69 @@ class Game(Frame):
                     self.board[i][j].configure(text=str(new_value), bg=e.BG_CELL, fg=e.FG_CELL)
         self.update_idletasks()
 
-    def pressed_key(self, event):
+    # Functions
+    def compress(self):
+        new_matrix = np.zeros((4, 4), dtype=int)
+        for i in range(e.SIZE):
+            position = 0
+            for j in range(e.SIZE):
+                if self.matrix[i][j] != 0:
+                    new_matrix[i][position] = self.matrix[i][j]
+                    position += 1
+        self.matrix = new_matrix
+
+    def combine(self):
+        for i in range(e.SIZE):
+            for j in range(e.SIZE - 1):
+                if self.matrix[i][j] != 0:
+                    if self.matrix[i][j] == self.matrix[i][j + 1]:
+                        self.matrix[i][j] *= 2
+                        self.matrix[i][j + 1] = 0
+
+    def gather_and_stack(self):
+        self.compress()
+        self.combine()
+        self.compress()
+
+    def reverse(self):
+        self.matrix = np.fliplr(self.matrix)
+
+    def transpose(self):
+        self.matrix = np.transpose(self.matrix)
+
+    def move(self, event):
         key = event.keysym
         print(event)
-        print(self.matrix)
-        if key in self.commands:
-            self.matrix = self.commands[key]
-            self.update_grid()
-            self.states.append(self.matrix)
-        print(self.matrix)
+        print(key)
+
+        if key == e.KEY_RESET:
+            self.matrix = functions.init_game()
+
+        if key == e.KEY_UP:
+            self.transpose()
+            self.gather_and_stack()
+            self.transpose()
+            print(self.matrix)
+
+        elif key == e.KEY_DOWN:
+            self.transpose()
+            self.reverse()
+
+            self.gather_and_stack()
+
+            self.reverse()
+            self.transpose()
+
+        elif key == e.KEY_LEFT:
+            self.gather_and_stack()
+
+        elif key == e.KEY_RIGHT:
+            self.reverse()
+            self.gather_and_stack()
+            self.reverse()
+
+        self.update_grid()
+        self.states.append(self.matrix)
 
 
 if __name__ == '__main__':
